a12e893c95bc20c4551ae5087e6f270102c00139
faketail <faketail@foxmail.com>
2019-04-13 15:56:09 +0800

    addMap
    添加了map模块

--- pages/index/1.js
+++ pages/index/1.js
@@ -14,6 +14,7 @@ Page({
   bindback: function(){
     BackgroundAudioManager.src = "http://www.ytmp3.cn/down/59964.mp3"
     BackgroundAudioManager.title = "Ark-Sound Horizon"
+    BackgroundAudioManager.coverImgUrl = "http://www.gepuwang.net/uploads/allimg/140726/13533WJ3-0.png"
     BackgroundAudioManager.play();
     BackgroundAudioManager.onPlay(() => {
       console.log("音乐播放开始");
@@ -21,5 +22,20 @@ Page({
   },
   stopPlay: function(){
     BackgroundAudioManager.pause();
+    
+      console.log("音乐播放暂停");
+    
+  },
+  onShareAppMessage: function () {
+    () => {
+      console.log("音乐开始");
+      console.log(BackgroundAudioManager.currentTime);
+    }
+    
+  },
+  inputFinish: function () {
+    () => {
+      console.log("音乐开始");
+    }
   }
 })
--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -11,5 +11,25 @@ Page({
         return util.formatTime(new Date(log))
       })
     })
+    
+  },
+  location: function () {
+    wx.getLocation({
+      type: 'gcj02', // 返回可以用于wx.openLocation的经纬度
+      success(res) {
+        console.log(res.latitude, res.longitude)//113.33476,22.987086
+        const latitude = 22.987086
+        const longitude = 113.33476
+        wx.openLocation({
+          latitude,
+          longitude,
+          scale: 18
+        })
+      }
+    })
+  },
+  mapPresentation: function () {
+    let mapcontext = wx.createMapContext(mapid, this)
   }
+  
 })
